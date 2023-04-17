3d5ad0b95546d1b51d9035c7dab691897ff6e5f2
Antti Alestalo <alestalo.antti@gmail.com>
2023-04-17 18:20:15 +0300

    fix script when coming from youtube.com to /shorts

--- shorts.js
+++ shorts.js
@@ -1,6 +1,10 @@
 let videoID;
 let lastUrl = new URL(document.location.href);
 
+const updateVideoId = (url) => {
+  videoID = url.pathname.slice(8);
+}
+
 const injectVideoLink = () => {
   const videoLink = document.getElementById("normal-video-link");
   if (!videoLink) {
@@ -21,12 +25,20 @@ const updateVideoId = (url) => {
 }
 
 //Run for the first time
-updateVideoId(lastUrl);
-injectVideoLink();
+if (location.href.includes("/shorts/")) {
+  updateVideoId(lastUrl);
+  injectVideoLink();
+}
 
 new MutationObserver(() => {
   let url = new URL(document.location.href);
-  if (url.href != lastUrl.href) {
+  //Return early if URL didn't change
+  if (url.href == lastUrl.href) {
+    return;
+  }
+
+  //Handle video link injection or removal
+  if (url.pathname.includes("/shorts/")) {
     lastUrl = url;
     updateVideoId(url);
     console.log(`videoID: ${videoID}`);
